f8d96424b433523b9c8067c7dbac34ed6124b217
Abuzar Munshi <zarwebcoders@gmail.com>
2026-06-28 01:23:59 +0530

    chore: adjust rate limiter limit in development

--- server/server.js
+++ server/server.js
@@ -63,7 +63,7 @@ app.use(morgan('dev')); // HTTP request logger
 // Rate limiting
 const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 500, // Limit each IP to 500 requests per `window` (here, per 15 minutes)
+  max: process.env.NODE_ENV === 'production' ? 500 : 100000, // Limit each IP to 500 requests per 15 mins in production, higher for local dev
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
